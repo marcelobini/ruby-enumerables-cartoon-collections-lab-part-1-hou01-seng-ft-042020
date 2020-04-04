3ff2c8169cb3cd580c9d27dad407c167d0481667
Marcelo Bini <marcelobini@gmail.com>
2020-04-04 20:56:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def list_dwarves(array)
   array.each_with_index do |value,index|
   # Print a numbered list of each element
   index=+1
-  puts "#{value}#{index}"
+  puts "#{index} #{value}"
 end
 end
